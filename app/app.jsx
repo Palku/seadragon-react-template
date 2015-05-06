5ddeddb9cb03d903d2dc1d68df00be7badc4a647
unknown <jlindbe2@942-jlindbe2-01.corp.corpcommon.com>
2015-05-06 13:20:17 +0200

    Test

--- app/app.jsx
+++ app/app.jsx
@@ -2,4 +2,4 @@
 'use strict'
 require(__dirname + '/app.css')
 var React = require('react')
-React.render(<h1>Honk</h1>, document.getElementById('content'))
+React.render(<h1>Honk!</h1>, document.getElementById('content'))
